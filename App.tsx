7d5d8e686112ce99e18c994107a0516a6e9ddc77
Ayush Goyal <goyalayu@amazon.com>
2020-07-14 17:19:30 +0530

    Add async thunk and init dbd method

--- App.tsx
+++ App.tsx
@@ -5,6 +5,15 @@ import { Provider } from 'react-redux';
 
 import store from './src/store';
 import PlacesStackScreen from './src/navigation/PlacesStackScreen';
+import { init } from './src/helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialized DB');
+  })
+  .catch(err => {
+    console.log('Init db failed', err);
+  });
 
 export default function App() {
   return (
--- src/store/placesSlice.ts
+++ src/store/placesSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import * as FileSystem from 'expo-file-system';
 
 import { Place } from '../models';
 
@@ -10,19 +11,35 @@ const initialState: PlacesState = {
   places: [],
 };
 
+export const addPlace = createAsyncThunk('places/addPlace', async (payload: Omit<Place, 'id'>) => {
+  const fileName = payload.imageUri.split('/').pop();
+  const newPath = `${FileSystem.documentDirectory}${fileName}`;
+
+  try {
+    await FileSystem.moveAsync({
+      from: payload.imageUri,
+      to: newPath,
+    });
+
+    payload.imageUri = newPath;
+    return payload;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
+
 const placesSlice = createSlice({
   name: 'places',
   initialState,
-  reducers: {
-    addPlace: (state, action: PayloadAction<Omit<Place, 'id'>>) => {
+  reducers: {},
+  extraReducers: builder =>
+    builder.addCase(addPlace.fulfilled, (state, action) => {
       state.places.push({
         id: new Date().toISOString(),
         ...action.payload,
       });
-    },
-  },
+    }),
 });
-
-export const { addPlace } = placesSlice.actions;
 
 export default placesSlice.reducer;
